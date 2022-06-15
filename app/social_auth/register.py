42c5a19b3c204975d47d33a8785addf96f80f8dc
haykserobyan <89manukyan>
2022-06-15 14:39:59 +0400

    change social authenticate

--- app/social_auth/register.py
+++ app/social_auth/register.py
@@ -32,6 +32,5 @@ def register_social_user(provider, user_id, email, first_name, last_name, avatar
         user.is_active = True
         user.auth_provider = provider
         user.save()
-        # create new user
         authenticate(email=email, password=SOCIAL_SECRET)
         return user.tokens()
