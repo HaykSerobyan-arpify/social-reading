0ac3d5e6c68b3fcef9124d586d00838b74dd41ad
haykserobyan <89manukyan>
2022-06-28 16:00:33 +0400

    chande like user

--- app/likes/models.py
+++ app/likes/models.py
@@ -8,27 +8,7 @@ from notification.models import Notification
 class Like(models.Model):
     quote = models.ForeignKey(Quote, on_delete=models.CASCADE, related_name="likes")
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    like = models.BooleanField(default=False)
 
     def __str__(self):
-        return f'{self.user} | {self.like}'
+        return f'{self.user} | {self.quote}'
 
-    def user_liked_quote(self, instance, *args, **kwargs):
-        like = instance
-        post = like.quote
-        sender = like.user
-
-        notify = Notification(post=post, sender=sender, user=self.quote.author, notification_type=1)
-        notify.save()
-
-    def user_unliked_quote(self, instance, *args, **kwargs):
-        like = instance
-        post = like.quote
-        sender = like.user
-
-        notify = Notification.objects.filter(post=post, sender=sender, notification_type=1)
-        notify.delete()
-
-
-post_save.connect(Like.user_liked_quote, sender=Like)
-post_delete.connect(Like.user_unliked_quote, sender=Like)
