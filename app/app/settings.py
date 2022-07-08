86788fde75a532b3fd627fec453825a6ce43f068
haykserobyan <89manukyan>
2022-07-08 16:27:29 +0400

    change upload files storage

--- app/app/settings.py
+++ app/app/settings.py
@@ -15,6 +15,7 @@ import environ
 from abc import ABC
 from datetime import timedelta
 from pathlib import Path
+
 from djongo.base import DatabaseWrapper
 from djongo.operations import DatabaseOperations
 
@@ -398,3 +399,14 @@ PYTESSERACT_LANGUAGES = (
     ('fra', 'French'),
     ('hin', 'Hindi'),
 )
+
+# AWS S3 STORAGE
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_S3_VERIFY = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
